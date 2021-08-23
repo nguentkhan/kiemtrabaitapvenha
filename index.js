ffbad0eb6993feaca50529d0de7b03aee1b7961e
nguentkhan <89307836+nguentkhan@users.noreply.github.com>
2021-08-23 12:12:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -28,14 +28,15 @@ function login(){
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
 
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
+firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-
-    window.alert("Error : " + errorMessage);
-
-    // ...
   });
 
 }
